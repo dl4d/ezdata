726dd60d698dbdf485ca83a05dc8b5aefdf5c78e
siltoon <siltoon1@gmail.com>
2019-02-27 22:44:16 +0100

    Last commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ezdata',
+      version='0.1',
+      description='Easy Data Wrapping for Keras',
+      url='http://github.com/dl4d/ezdata',
+      author='Fab.Da',
+      author_email='deeplearningformation@gmail.com',
+      license='GPL3.0',
+      packages=['ezdata'],
+      zip_safe=False)
